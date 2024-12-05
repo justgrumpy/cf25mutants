800dd4bc931bb27fe71db29e49d44f0e75b95f35
justgrumpy <bbatronis@yahoo.com>
2024-12-05 15:44:00 -0700

    setting up pre stryker

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
-jestConfig.testPathIgnorePatterns = [...jestConfig.testPathIgnorePatterns, "<rootDir>/.stryker-tmp/"];
+
 module.exports = {
     ...jestConfig,
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
